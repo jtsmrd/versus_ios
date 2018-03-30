a50dab4e5a8bda967b981f03013bc5e64bdb221c
JT Smrdel <jtsmrdel@JTs-MacBook-Pro.home>
2018-03-30 14:08:22 -0400

    Initial commit

---
+++ Versus/Versus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Versus
+//
+//  Created by JT Smrdel on 3/30/18.
+//  Copyright © 2018 VersusTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
