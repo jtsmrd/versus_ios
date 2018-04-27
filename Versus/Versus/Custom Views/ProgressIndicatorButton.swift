38c6079dc4981b2701692dcbcb75f1705ad17b15
JT Smrdel <jtsmrdel@gmail.com>
2018-04-27 18:22:39 -0400

    Created progress indicator button and added small image versions to competition entry and competition. Also added a custom countdown timer

---
+++ Versus/Versus/Custom Views/ProgressIndicatorButton.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressIndicatorButton.swift
+//  Versus
+//
+//  Created by JT Smrdel on 4/27/18.
+//  Copyright © 2018 VersusTeam. All rights reserved.
+//
+
+import UIKit
+
+class ProgressIndicatorButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Versus/Versus/Utilities/CountdownTimer.swift
@@ -0,0 +1,9 @@
+//
+//  CountdownTimer.swift
+//  Versus
+//
+//  Created by JT Smrdel on 4/27/18.
+//  Copyright © 2018 VersusTeam. All rights reserved.
+//
+
+import Foundation
